48539d71092c4d163b5bef08be46a705762b9411
Yizlace Uzcategui Domero <yizlace@MacBook-Pro-de-Yizlace.local>
2025-08-28 15:52:11 -0400

    CONFIG Rutas hijas del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,19 +5,21 @@ export const routes: Routes = [
   {
     path: 'dashboard',
       loadComponent: () =>
-        import('./gifs/pages/dashboard-page/dashboard-page.component')
-  },
+        import('./gifs/pages/dashboard-page/dashboard-page.component'),
 
-  {
-    path: 'trending',
-      loadComponent: () =>
-        import('./gifs/pages/trending-page/trending-page.component')
-  },
+      children: [
+        {
+          path: 'trending',
+            loadComponent: () =>
+              import('./gifs/pages/trending-page/trending-page.component')
+        },
 
-  {
-    path: 'search',
-      loadComponent: () =>
-        import('./gifs/pages/search-page/search-page.component')
+        {
+          path: 'search',
+            loadComponent: () =>
+              import('./gifs/pages/search-page/search-page.component')
+        },
+      ]
   },
 
   {
