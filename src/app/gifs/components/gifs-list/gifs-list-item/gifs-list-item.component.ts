3235a915b1a3ae840aa1c9d9a2684fa8564a7d0d
Yizlace Uzcategui Domero <yizlace@MacBook-Pro-de-Yizlace.local>
2025-08-28 21:27:06 -0400

    se crean la gifs-list-item

---
+++ src/app/gifs/components/gifs-list/gifs-list-item/gifs-list-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-list-item',
+  imports: [],
+  templateUrl: './gifs-list-item.component.html',
+})
+export class GifsListItemComponent { }
--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { GifsListItemComponent } from "../../components/gifs-list/gifs-list-item/gifs-list-item.component";
+import { GifsListComponent } from '../../components/gifs-list/gifs-list.component';
+
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent { }
