2efc18bc63a86c9aa0bc94a9f227bd943aea93f1
Yizlace Uzcategui Domero <yizlace@MacBook-Pro-de-Yizlace.local>
2025-09-01 17:11:09 -0400

    creando el buscador

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { GifsListComponent } from "../../components/gifs-list/gifs-list.component";
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './search-page.component.html',
 })
-export  default class SearchPageComponent { }
+export  default class SearchPageComponent {
+  onSearch(query: string){
+    console.log({query});
+  }
+ }
